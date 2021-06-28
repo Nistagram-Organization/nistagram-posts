e05ced084e681baa72b4190bac3dac4da8041ff3
Danijel Radulović <master.daca09@gmail.com>
2021-06-28 11:57:03 +0200

    #20 Implement backend methods for reporting a post and add workflows

--- src/repositories/post/post_repository.go
+++ src/repositories/post/post_repository.go
@@ -1,13 +1,17 @@
 package post
 
 import (
+	"fmt"
 	"github.com/Nistagram-Organization/nistagram-shared/src/datasources"
 	"github.com/Nistagram-Organization/nistagram-shared/src/model/post"
+	"github.com/Nistagram-Organization/nistagram-shared/src/utils/rest_error"
 	"gorm.io/gorm"
 )
 
 type PostRepository interface {
 	GetAll() []post.Post
+	Get(uint) (*post.Post, rest_error.RestErr)
+	Update(*post.Post) rest_error.RestErr
 }
 
 type postsRepository struct {
@@ -26,4 +30,22 @@ func (p *postsRepository) GetAll() []post.Post {
 		return []post.Post{}
 	}
 	return collection
+}
+
+func (p *postsRepository) Get(id uint) (*post.Post, rest_error.RestErr) {
+	post := post.Post{
+		ID: id,
+	}
+	if err := p.db.Take(&post, post.ID).Error; err != nil {
+		fmt.Sprintln(err)
+		return nil, rest_error.NewNotFoundError(fmt.Sprintf("Error when trying to get post with id %d", post.ID))
+	}
+	return &post, nil
+}
+
+func (p *postsRepository) Update(post *post.Post) rest_error.RestErr {
+	if err := p.db.Save(post).Error; err != nil {
+		return rest_error.NewInternalServerError("Error when trying to update post", err)
+	}
+	return nil
 }
